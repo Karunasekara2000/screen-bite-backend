437ca1ff0fe926d9d651131aebdbe7fd70a74870
lahiruSandaruwan20 <lahiru.20210390@iit.ac.lk>
2025-07-24 16:02:37 +0530

    Project Initialise.

---
+++ screenbite-backend/src/main/java/com/screenbite/backend/ScreenbiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.screenbite.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenbiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenbiteBackendApplication.class, args);
+	}
+
+}
